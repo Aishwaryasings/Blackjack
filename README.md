491dda2c985585a02ba7cf6569cb2eee3866554b
Aishwarya Singh <as2778@cornell.edu>
2022-10-27 11:01:32 -0400

    brainstorm for first version

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Blackjack
 This is a tiny blackjack game I made 
+
+Iterations
+
+First round = win if get over 18 but less than 
+21, ace is just one 
+
+
--- blackjack.py
+++ blackjack.py
@@ -1,2 +1,12 @@
-import string
+#All the things I need to make a blackjack game
+#1) a deck of cards - array of 1 to 10 with enough 10s to correspond to kings and queens and jacks)
+#3) player points - int
+#4) player hit - boolean 
+
+#gotta keep going through the game until player stops hitting or points > 18 or less than 21
+#ask player to play hit or stay 
+#pick random card from deck 
+#calculate points
+# if greater than 18 < 21: won! break 
+# if greater than 21: lose! break
 
